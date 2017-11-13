c88bb808fdb30082399f5f01756fa67aaf7388ce
lyzerk <ozdemirali@itu.edu.tr>
2017-11-13 23:46:59 +0300

    test

---
+++ src/application.java
@@ -0,0 +1,5 @@
+public class application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/master.java
@@ -0,0 +1,5 @@
+public class master {
+    public void run() {
+        System.out.println("test");
+    }
+}
